a8838492c62b57d8ba9b685e835157f3e7399c94
Ramesh <rforramesh409@gmail.com>
2025-04-07 04:14:04 +0530

    JS : Remove Duplicate from Array

---
+++ HTML practice/script.js
@@ -0,0 +1,9 @@
+
+let arr = [
+    { ID: 78, Name: "factory1", Age: 25, City: "New York", IsActive: 1 },
+    { ID: 79, Name: "factory2", Age: 30, City: "Chicago", IsActive: 1 },
+    { ID: 80, Name: "factory3", Age: 25, City: "New York", IsActive: 1 },
+    { ID: 81, Name: "factory4", Age: 35, City: "Boston", IsActive: 1 },
+    { ID: 82, Name: "factory5", Age: 30, City: "Chicago", IsActive: 1 }
+];
+
